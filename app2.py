8b82d9a4a3b13149bc914f80b24d0d69dabb2c2b
Sum1NamedJon <17915475+Sum1Solutions@users.noreply.github.com>
2023-07-04 16:32:36 +0000

    fixes, etc.

--- app2.py
+++ app2.py
@@ -25,10 +25,13 @@ def make_chat_completion_request(messages):
 
 def run_chat_completion():
     messages = [{"role": "user", "content": "Hello world"}]  # Initialize with a user message
-    termination_conditions = ["thank you", "bye", "quit"]  # Define termination conditions
+    termination_conditions = ["bye", "quit"]  # Define termination conditions
+    max_response_length = 100  # Maximum number of tokens to display in a single print output
+
+    num_tokens = 0  # Initialize the variable outside the loop
 
     while True:
-        user_input = input("User: ")
+        user_input = input("Enter your question here ('bye' or 'quit' to end session): ")
         messages.append({"role": "user", "content": user_input})
 
         if any(condition in user_input.lower() for condition in termination_conditions):
@@ -39,9 +42,39 @@ def run_chat_completion():
 
         # Extract assistant response and process it if needed
         assistant_response = chat_completion_result["choices"][0]["message"]["content"]
-        print("Assistant:", assistant_response)
+
+        # Print assistant response
+        print(f"Assistant:")
+
+        # Split long responses into multiple print statements
+        response_tokens = assistant_response.split()
+        num_tokens = len(response_tokens)
+        num_prints = (num_tokens - 1) // max_response_length + 1
+        for i in range(num_prints):
+            start = i * max_response_length
+            end = (i + 1) * max_response_length
+            print(' '.join(response_tokens[start:end]))
 
         messages.append({"role": "assistant", "content": assistant_response})
+
+        # Ask whether to continue or quit if response is long
+        if num_tokens > max_response_length:
+            user_choice = input("There's more. Continue? (yes/no): ")
+            if user_choice.lower() == "no":
+                break
+
+    # Calculate the total cost (this is just a rough estimate, consult OpenAI's pricing for accurate costs)
+    total_tokens = sum(len(message["content"].split()) for message in messages if message["role"] == "assistant")
+    cost_per_token = 0.0000002  # Replace with the actual cost per token from OpenAI
+    total_cost = total_tokens * cost_per_token
+
+    # Print metadata
+    print(f"Total tokens: {total_tokens}")
+    if num_tokens:
+        print(f"Response length: {num_tokens} tokens")
+    # Print total cost
+    print(f"Total Cost: ${total_cost:.9f}")
+
 
 if __name__ == "__main__":
     run_chat_completion()
